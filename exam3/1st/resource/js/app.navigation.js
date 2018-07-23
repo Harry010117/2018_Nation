f98207a8358181d1212d077fb0e7455aa26318c1
Harry <harry010117@gmail.com>
2018-07-23 14:38:36 +0900

    asdf

--- exam3/1st/resource/js/app.navigation.js
+++ exam3/1st/resource/js/app.navigation.js
@@ -8,35 +8,53 @@ class Navigation {
 	static init () {
 
 		// jQuery selector minimization
+		const page = $('page')
+		const url = new URL(location.href) || null
 		Navigation.main    = $('.main')
 		Navigation.sub     = $('.sub')
-		Navigation.page    = $('.page')
+		Navigation.page    = page
 		Navigation.gnb     = $('.gnb')
 		Navigation.len     = Navigation.gnb.find('li').length
 		Navigation.section = Navigation.page.find('>section')
-		Navigation.nowPage = -1
+		Navigation.current = url && url.serachParams.get('page') !== null ? parseInt(url.serachParams.get(page)) : -1
+
+		if (Navigation.current == -1) {
+			Navigation.main.addClass('active')
+			new Animation({obj:$('.main, .arrow')})
+		}else {
+			Navigation.sub.addClass('active')
+			Navigation.section.eq(Navigation.nowPage).('active')
+			new Animation({obj:$('.sub-default, .arrow, .page>section.active')})
+		}
 	}
 
 	// go to main page
 	static goToMain () {
-		Navigation.nowPage = -1
-		Navigation.sub.removeClass('active')
-		Navigation.page.find('>section.active').removeClass('active')
-		Navigation.main.addClass('active')
+		const sub = Navigation.sub
+		const main = Navigation.main
+		const target = sub.find('.page>section, .sub-default')
+		const closeSub = () => {
+			sub.fadeOut(500, () => {
+				target.removeClass('active').removeAttr('style')
+				sub.removeClass('active').removeAttr('style')
+				main.fadeIn(300, () => {
+					main.addClass('active')
+					new Animation({obj:main})
+				})
+			})
+		}
 	}
 
 	// go to selected sub page
-	static goToPage () {
-		Navigation.goToPageReal($(this).index())
-	}
+	static goToPage () {Navigation.goToPageReal($(this).index())}
 
 	// go to page
 	static goToPageReal (num) {
 		// variable set
 		const main = Navigation.main,
-			  sub  = Navigation.sub,
-			  page = Navigation.page,
-			  gnb  = Navigation.gnb
+		sub  = Navigation.sub,
+		page = Navigation.page,
+		gnb  = Navigation.gnb
 
 		if (Navigation.nowPage === -1) {
 			main.removeClass('active')
@@ -54,7 +72,7 @@ class Navigation {
 	static goToArrow () {
 		// variable set 
 		const _this = $(this),
-		      len   = Navigation.len
+		len   = Navigation.len
 		let   num   = Navigation.nowPage
 		num = _this.hasClass('left') ? num - 1 : num + 1
 		if (num == -1 || num >= len) {
